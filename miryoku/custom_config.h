35174b75289759fab847a1165886e90af97605fc
smp4488 <smp4488@aol.com>
2023-08-24 10:14:00 -0400

    flipping layers and inverting nav

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,2 +1,9 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_EXTRA_COLEMAKDH
+#define MIRYOKU_NAV_INVERTEDT
+#define MIRYOKU_CLIPBOARD_WIN
+#define MIRYOKU_LAYERS_FLIP
